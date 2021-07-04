051087df10304d30b34aca23abf6d2fc6a2ccf2e
syeong0204 <syeong0204@gmail.com>
2021-07-03 21:15:45 -0400

    answered questions with database using postgres

--- sql_queries.sql
+++ sql_queries.sql
@@ -76,3 +76,46 @@ SELECT * FROM "Department_Manager"
 SELECT * FROM "Employee"
 SELECT * FROM "Salary"
 SELECT * FROM "Titles"
+1
+SELECT e.emp_no, e.last_name, e.first_name,e.sex, s.salary FROM "Employee" as e 
+Inner Join "Salary" as s
+on s.emp_no = e.emp_no;
+2
+SELECT e.first_name, e.last_name, e.hire_date FROM "Employee" as e
+WHERE e.hire_date BETWEEN '1986-01-01' and '1986-12-31'
+3
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM "Department" as d
+Inner join "Department_Manager" as dm on dm.dept_no = d.dept_no
+Inner join "Employee" as e on e.emp_no = dm.emp_no;
+
+4
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Employee" as e
+JOIN "Department_Employee" as de ON de.emp_no = e.emp_no
+JOIN "Department" as d on d.dept_no = de.dept_no;
+5
+
+SELECT last_name, first_name, sex 
+FROM "Employee"
+WHERE first_name = "%H" and last_name = "%B";
+
+6
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Employee" as e
+INNER JOIN "Department_Employee" as de on de.emp_no = e.emp_no
+Inner Join "Department" as d on d.dept_no = de.dept_no
+WHERE dept_name = 'Sales';
+
+7
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Employee" as e
+INNER JOIN "Department_Employee" as de on de.emp_no = e.emp_no
+Inner Join "Department" as d on d.dept_no = de.dept_no
+WHERE dept_name IN ('Sales', 'Development');
+
+8
+SELECT last_name, count(last_name)
+FROM "Employee"
+GROUP BY last_name
+ORDER BY COUNT(last_Name) DESC;
